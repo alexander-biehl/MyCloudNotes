8b1f83c34101985e9b69cab88042976a0c7bda87
Alex Biehl <biehl.al@gmail.com>
2024-03-10 17:13:04 -0400

    bootstrapped project from spring initializr

---
+++ src/main/java/com/alexbiehl/mycloudnotes/MycloudnotesApplication.java
@@ -0,0 +1,13 @@
+package com.alexbiehl.mycloudnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycloudnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycloudnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexbiehl/mycloudnotes/MycloudnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexbiehl.mycloudnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycloudnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
