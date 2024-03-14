f750932130a98c59a173dbf480d6750409ea3114
Alex Biehl <biehl.al@gmail.com>
2024-03-14 12:34:24 -0400

    separated health-check into its own controller

--- src/main/java/com/alexbiehl/mycloudnotes/controller/NotesController.java
+++ src/main/java/com/alexbiehl/mycloudnotes/controller/NotesController.java
@@ -1,9 +1,12 @@
 package com.alexbiehl.mycloudnotes.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alexbiehl.mycloudnotes.dto.Note;
 import com.alexbiehl.mycloudnotes.repository.NotesRepository;
 
 @RestController
@@ -12,9 +15,9 @@ public class NotesController {
     @Autowired
     NotesRepository repository;
 
-    @GetMapping("/health-check")
-    public String healthCheck() {
-        return "OK";
-    }
+    // @GetMapping("/notes")
+    // public List<Note> GetNotes() {
+    // List<Note> notes = repository.findAll();
+    // }
 
 }
