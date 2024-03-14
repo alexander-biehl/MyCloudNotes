91216328da09d9ad1f0cbb4a48c4009d68bb4e04
Alex Biehl <biehl.al@gmail.com>
2024-03-14 12:33:58 -0400

    finally got docker to init the database correctly, creating the table as well

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,10 @@
+
+-- CREATE USER notes_user;
+
+-- GRANT ALL PRIVILEGES ON DATABASE mycloudnotes TO notes_user;
+
 CREATE TABLE IF NOT EXISTS notes (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    id uuid DEFAULT gen_random_uuid(),
     content varchar(255),
+    PRIMARY KEY (id)
 );
