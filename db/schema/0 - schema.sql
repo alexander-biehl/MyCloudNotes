cd46c1e0df3cd36784b690619f0e9caa75d3c51b
Alex Biehl <biehl.al@gmail.com>
2024-04-01 22:42:34 -0400

    Updated DDL for new schema, and updated compose file to read in these changes and execute them if a volume doesn't already exist.

--- db/schema/0 - schema.sql
+++ db/schema/0 - schema.sql
@@ -3,19 +3,20 @@
 
 -- GRANT ALL PRIVILEGES ON DATABASE mycloudnotes TO notes_user;
 
-CREATE TABLE IF NOT EXISTS notes (
-    id uuid DEFAULT gen_random_uuid(),
-    title varchar(32) NOT NULL,
-    content varchar(255),
-    user_id uuid REFERENCES users (id),
-    PRIMARY KEY (id)
-);
 
 CREATE TABLE IF NOT EXISTS users (
     id uuid DEFAULT gen_random_uuid(),
     username varchar(50) NOT NULL,
     password varchar(500) NOT NULL,
-    active boolean DEFAULT 0 NOT NULL,
+    active boolean DEFAULT true NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS notes (
+    id uuid DEFAULT gen_random_uuid(),
+    title varchar(32) NOT NULL,
+    content varchar(255),
+    user_id uuid REFERENCES users (id),
     PRIMARY KEY (id)
 );
 
