831fde07992b6f16510cf3cf135d17a76bd20534
Alex Biehl <biehl.al@gmail.com>
2024-04-05 11:45:55 -0400

    added hardcoded data for DB testing with

---
+++ src/test/java/com/alexbiehl/mycloudnotes/utils/TestConstants.java
@@ -0,0 +1,17 @@
+package com.alexbiehl.mycloudnotes.utils;
+
+import java.util.UUID;
+
+public class TestConstants {
+
+    public static final String USER_ROLE_NAME = "ROLE_USER";
+    public static final UUID USER_ROLE_ID = UUID.fromString("b1f1ce4f-f294-4994-befc-0673b8771e77");
+
+    public static final String ADMIN_ROLE_NAME = "ROLE_ADMIN";
+    public static final UUID ADMIN_ROLE_ID = UUID.fromString("3dd44c43-ff43-4891-8a14-c36a8fe72347");
+
+    public static final UUID TEST_USER_ID = UUID.fromString("5c553179-71b1-4c85-842e-b6ff67dc8e61");
+    public static final UUID TEST_ADMIN_ID = UUID.fromString("bfd6ada0-2b46-4971-a314-d5abd7b7ebb1");
+
+    public static final UUID TEST_NOTE_ID = UUID.fromString("6608cd99-e2f7-4dca-b4dc-9a40a21a25b9");
+}
