136f8366b48b4452545a9d2d24d062b661b54fae
Alex Biehl <biehl.al@gmail.com>
2024-04-05 13:44:38 -0400

    added some more test data and indexes

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -6,11 +6,12 @@
 
 CREATE TABLE IF NOT EXISTS users (
     id uuid DEFAULT gen_random_uuid(),
-    username varchar(50) NOT NULL UNIQUE,
+    username varchar(50) NOT NULL,
     password varchar(500) NOT NULL,
     active boolean DEFAULT true NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX IF NOT EXISTS  users_username_index ON users (username);
 
 CREATE TABLE IF NOT EXISTS notes (
     id uuid DEFAULT gen_random_uuid(),
@@ -19,12 +20,14 @@ CREATE TABLE IF NOT EXISTS notes (
     user_id uuid REFERENCES users (id),
     PRIMARY KEY (id)
 );
+CREATE INDEX IF NOT EXISTS notes_title_idx ON notes (title);
 
 CREATE TABLE IF NOT EXISTS roles (
     id uuid DEFAULT gen_random_uuid(),
-    name varchar(32) NOT NULL UNIQUE,
+    name varchar(32) NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX IF NOT EXISTS roles_name_idx ON roles (name);
 
 CREATE TABLE IF NOT EXISTS user_roles (
     user_id uuid REFERENCES users (id),
